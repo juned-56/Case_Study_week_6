be64959511df612f6cf6d90b585386251fa9ad9c
Juned Khan <73770571+juned-56@users.noreply.github.com>
2022-03-25 15:24:38 +0530

    Update MobileDao.java

--- MobileManagementApplication/src/main/java/com/mobile/dao/MobileDao.java
+++ MobileManagementApplication/src/main/java/com/mobile/dao/MobileDao.java
@@ -3,7 +3,9 @@ package com.mobile.dao;
 import org.springframework.data.repository.CrudRepository;
 
 import com.mobile.entity.Mobile;
-
+//Crud Repository is the base interface and it acts as a marker interface
+//it provides crud functions
+//CrudRepository<Entity_Class<Mobile>, ID Wrapper_Walss<Integer>>
 public interface MobileDao extends CrudRepository<Mobile, Integer>{
 
 }
